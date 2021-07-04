50a52dae93890e9c72e261fc97f9cb25798c1eaf
liviolet <2929724643@qq.com>
2021-07-04 11:05:25 +0800

    IDEA工程文件
    工程文件

--- src/Offer_05.java
+++ src/Offer_05.java
@@ -1,2 +1,21 @@
 public class Offer_05 {
+    String replaceSpace(String s){
+        StringBuffer sb = new StringBuffer(s);
+        int x = s.length();
+        for(int i = 0 ; i < x; i++){
+            if (sb.charAt(i) == ' '){
+                sb.insert(i,"%20");
+                sb.deleteCharAt(i+3);
+                x += 2;
+            }
+        }
+        String result = sb.toString();
+        return result;
+    }
+
+    public static void main(String[] args) {
+        Offer_05 offer_05 = new Offer_05();
+        String result = offer_05.replaceSpace("      ");
+        System.out.println(result);
+    }
 }
