8c17652b87ed2508b3b53cf67003972678b62ce6
liviolet <2929724643@qq.com>
2021-07-07 14:42:18 +0800

    Day_03

--- src/Offer_09.java
+++ src/Offer_09.java
@@ -1,18 +1,30 @@
+import java.util.Stack;
+
 public class Offer_09 {
 }
 
 class CQueue {
+    Stack<Integer> A,B ;
 
     public CQueue() {
-
+        A = new Stack();
+        B = new Stack();
     }
 
     public void appendTail(int value) {
-
+        A.push(value);
     }
 
     public int deleteHead() {
-
+        if(!B.isEmpty()){
+            return B.pop();
+        }
+        if (A.isEmpty()){
+            return -1;
+        }
+        while(!A.isEmpty())
+            B.push(A.pop());
+        return B.pop();
     }
 }
 
